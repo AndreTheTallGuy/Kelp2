6da89ebcbfa99839786ac75c06f1613d02bd3c75
Andre Entrekin <andre.entrekin@gmail.com>
2021-01-25 11:57:01 -0600

    Spring Boot Starter

---
+++ src/test/java/com/Kelp2/kelp/KelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kelp2.kelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
