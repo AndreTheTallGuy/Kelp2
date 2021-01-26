d3447a93736363c85dd1f80746e4caaa5c3ae87b
Andre Entrekin <andre.entrekin@gmail.com>
2021-01-26 12:25:23 -0600

    added pom and other files

---
+++ server/src/main/java/com/Kelp2/kelp/KelpApplication.java
@@ -0,0 +1,13 @@
+package com.Kelp2.kelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelpApplication.class, args);
+	}
+
+}
