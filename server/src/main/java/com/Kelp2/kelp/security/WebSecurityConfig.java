994b019e0c1940a647a6b8a6090ca57bf6a5aeb5
Andre Entrekin <andre.entrekin@gmail.com>
2021-02-27 13:49:23 -0600

    changed root

--- server/src/main/java/com/Kelp2/kelp/security/WebSecurityConfig.java
+++ server/src/main/java/com/Kelp2/kelp/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,8 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/*")
                 .authenticated()
                 .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+//                .jwt();
     }
 
 
